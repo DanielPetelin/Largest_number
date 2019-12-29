a48f85994d3436efba5031364067bbd70b5e475c
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-29 11:00:07 +0300

    a > b

--- js/script.js
+++ js/script.js
@@ -4,5 +4,11 @@ var B = 63;
 function countNumber(a, b) {
     a = A;
     b = B;
+
+    if (a > b) {
+        console.log(a + ' > ' + b);
+    } else {
+        console.log(a + '<' + b);
+    }
 };
 countNumber();
